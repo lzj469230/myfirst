1f378f6411260b32beb0772c72e26c45c9c00259
lzj469230 <lzj469230@163.com>
2018-11-15 20:18:45 +0800

    第二个版本

---
+++ newWeb/p01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+
+void sys_err(const char *str)
+{
+	perror(str);
+	exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
